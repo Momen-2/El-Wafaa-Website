d6844dcdb72879a6642ef19d2a40a4e754b60d53
Momen <momenprogrammer@gmail.com>
2024-01-29 17:57:07 +0200

    Add pillow to requirements.txt file

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = ["*"]
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = ["https://web-production-0bbb1.up.railway.app"]
+
 # if DEBUG == False:
 #     SECURE_SSL_REDIRECT = True
 
