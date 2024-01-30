5e47a868c525be8b8a8b73eb2ec2e092b44d7429
Momen <momenprogrammer@gmail.com>
2024-01-30 08:54:23 +0200

    Add AWS_LOCATION

--- django_project/settings.py
+++ django_project/settings.py
@@ -105,7 +105,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
-# AWS_LOCATION = ""
+AWS_LOCATION = "el-waffaa-website/"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
